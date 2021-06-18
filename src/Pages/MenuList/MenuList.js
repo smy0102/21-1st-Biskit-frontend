2c3db02b1b0c60180b33c7f6c42cd5248c358eef
mac <mac@Bingbing.local>
2021-06-18 10:32:41 +0900

    Add: [21기 권오현] 페이지네이션 버튼 수정

--- src/Pages/MenuList/MenuList.js
+++ src/Pages/MenuList/MenuList.js
@@ -47,7 +47,6 @@ class MenuList extends React.Component {
       fetch(`http://10.58.0.85:8000/products${this.props.location.search}`)
         .then(res => res.json())
         .then(data => {
-          console.log(data);
           this.setState({
             snackList: data.result,
           });
@@ -211,9 +210,11 @@ class MenuList extends React.Component {
             <button type="button" onClick={e => this.pageNum(0)}>
               1
             </button>
-            <button type="button" onClick={e => this.pageNum(1)}>
-              2
-            </button>
+            {this.state.snackList.length > 20 && (
+              <button type="button" onClick={e => this.pageNum(1)}>
+                2
+              </button>
+            )}
           </div>
         </section>
       </>
